0e702391c6404a45f8add9d18e4b59fcf3b36786
osmarrjr <osmar.rjr@hotmail.com>
2021-08-24 16:12:32 -0300

    Configurando Webpack e WebPack Server

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlkWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        // aqui era contentBase: path.resolve(__dirname, 'public'),
+        static: path.resolve(__dirname, 'public'),
+    },
+    plugins: [
+        new HtmlkWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ],
+    }
+};
